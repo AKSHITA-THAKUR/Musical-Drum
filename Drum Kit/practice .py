0c5145da3a9878d517934528878375e1fd05548a
AKSHITA-THAKUR <109726012+AKSHITA-THAKUR@users.noreply.github.com>
2023-10-11 22:47:10 +0530

    Add files via upload

---
+++ Drum Kit/practice .py
@@ -0,0 +1,12 @@
+a=int(input("enter the value of a"))
+c=int(input("enter the value of b"))
+b=int(input("enter the value of c"))
+print(a)
+print(b)
+print(c)
+if a>b and a>c :
+    print(" a is greater")
+elif b>a and b>c :
+    print( " b is greater ")
+else :
+    print("c is greater")
